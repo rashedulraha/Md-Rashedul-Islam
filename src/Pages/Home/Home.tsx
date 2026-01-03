b92bd30529fc81bedad37d9b5e21d72a1889c26a
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-03 12:36:32 +0600

    update home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,12 +4,13 @@ import HeroBanner from "../shared/HeroBanner/HeroBanner";
 
 const Home = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden bg-background selection:bg-primary selection:text-primary-foreground">
+    <div className="relative min-h-screen lg:h-screen w-full overflow-y-auto lg:overflow-hidden bg-background selection:bg-primary selection:text-primary-foreground">
       <Animation />
       <Navbar />
-      <div className="relative z-10 h-full w-full flex items-center justify-center pt-16">
+
+      <main className="relative z-10 min-h-[calc(100vh-64px)] w-full flex items-center justify-center pt-16 px-4">
         <HeroBanner />
-      </div>
+      </main>
     </div>
   );
 };
