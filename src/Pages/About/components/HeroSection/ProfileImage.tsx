968b5ea69da99849a747a520fa22661875062faa
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-21 21:29:18 +0600

    image problem slove

--- src/Pages/About/components/HeroSection/ProfileImage.tsx
+++ src/Pages/About/components/HeroSection/ProfileImage.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import image from "../../assets/rashedul.jpeg";
+import image from "../../../../assets/rashedul.jpeg";
 
 const itemVariants = {
   hidden: { opacity: 0, y: 10 },
