c58ddc4a561d8449918e1ecda1647602ef8c8d87
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-27 02:31:36 +0600

    adding typescript issue

--- src/components/ui/spotlight.tsx
+++ src/components/ui/spotlight.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { cn } from "@/lib/utils";
 
 type SpotlightProps = {
@@ -10,13 +9,12 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] w-[138%] lg:w-[84%] opacity-0",
-        className
+        "animate-spotlight pointer-events-none absolute z-1  h-[169%] w-[138%] lg:w-[84%] opacity-0",
+        className,
       )}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 3787 2842"
-      fill="none"
-    >
+      fill="none">
       <g filter="url(#filter)">
         <ellipse
           cx="1924.71"
@@ -25,8 +23,7 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           ry="273.501"
           transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
           fill={fill || "white"}
-          fillOpacity="0.21"
-        ></ellipse>
+          fillOpacity="0.21"></ellipse>
       </g>
       <defs>
         <filter
@@ -36,19 +33,16 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           width="3785.16"
           height="2840.26"
           filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
+          colorInterpolationFilters="sRGB">
           <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
           <feBlend
             mode="normal"
             in="SourceGraphic"
             in2="BackgroundImageFix"
-            result="shape"
-          ></feBlend>
+            result="shape"></feBlend>
           <feGaussianBlur
             stdDeviation="151"
-            result="effect1_foregroundBlur_1065_8"
-          ></feGaussianBlur>
+            result="effect1_foregroundBlur_1065_8"></feGaussianBlur>
         </filter>
       </defs>
     </svg>
